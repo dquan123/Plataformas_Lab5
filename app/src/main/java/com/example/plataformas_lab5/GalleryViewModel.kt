20f9de5a82cdefec57dd9162a5bb7427db6c6c49
Diego Quan <dquan@delicatessen502.com>
2025-08-21 11:14:14 -0600

    Archivo que agrega las imagenes

---
+++ app/src/main/java/com/example/plataformas_lab5/GalleryViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.laboratorio5
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class GalleryViewModel : ViewModel(){
+
+    //Se guardan las imagenes seleccionadas
+    private val img = MutableStateFlow<List<Uri>>(emptyList())
+    val imagenes: StateFlow<List<Uri>> = img
+
+
+    //Funcion para agregar una nueva imagen a la lista
+    fun agregarImagen(uri: Uri){
+        img.value = img.value + uri
+    }
+}
