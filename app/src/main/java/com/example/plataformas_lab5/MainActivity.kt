6691cbce8c55b50cf39bbac84dff42803e152b13
Diego Quan <dquan@delicatessen502.com>
2025-08-21 11:11:59 -0600

    Archivo del main

--- app/src/main/java/com/example/plataformas_lab5/MainActivity.kt
+++ app/src/main/java/com/example/plataformas_lab5/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.plataformas_lab5
+package com.example.laboratorio5
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,37 +11,43 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.plataformas_lab5.ui.theme.Plataformas_Lab5Theme
+import com.example.laboratorio5.ui.theme.Laboratorio5Theme
+import android.net.Uri
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 
 class MainActivity : ComponentActivity() {
+
+    //Conectar con ViewModel
+    private val viewModel: GalleryViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //Registrar el photo picker
+        val pickFotos = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+                uri: Uri? -> uri?.let{
+            viewModel.agregarImagen(it) //Se agrega la imagen al ViewModel
+        }
+        }
+
         setContent {
-            Plataformas_Lab5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Laboratorio5Theme {
+                Surface(color = MaterialTheme.colorScheme.background){
+                    GalleryScreen(
+                        viewModel = viewModel,
+                        onPickImage = {
+                            pickFotos.launch(
+                                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                            )
+                        }
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Plataformas_Lab5Theme {
-        Greeting("Android")
     }
 }
